ac5920b1bce8c60487a3b79ee2bebbb03dfe9add
Eren <eren.duran@colins.com>
2025-01-17 00:12:39 +0300

    mark as read

---
+++ app/actions/markMessageAsRead.js
@@ -0,0 +1,30 @@
+"use server";
+import connectDb from "../../config/database";
+import Message from "../../models/Message";
+import { getSessionUser } from "../../utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+
+async function markMessageAsRead(messageId) {
+  await connectDb();
+
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("UserId is required");
+  }
+
+  const { userId } = sessionUser;
+  const message = await Message.findById(messageId);
+  if (!message) throw new Error("Message not found");
+
+  if (message.recipient.toString() !== userId) {
+    throw new Error("Unauthorized");
+  }
+
+  message.read = !message.read;
+  revalidatePath("/messages", "page");
+  await message.save();
+
+  return message.read;
+}
+
+export default markMessageAsRead;
