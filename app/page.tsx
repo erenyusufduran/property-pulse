e52a0275a5b61f4d7aff71ea40675f8afd22fe51
Eren <eren.duran@colins.com>
2025-01-19 17:29:05 +0300

    featured properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-import Hero from '../components/Hero';
-import InfoBoxes from '../components/InfoBoxes';
-import HomeProperties from '../components/HomeProperties';
-import connectDb from '../config/database';
+import Hero from "../components/Hero";
+import InfoBoxes from "../components/InfoBoxes";
+import HomeProperties from "../components/HomeProperties";
+import connectDb from "../config/database";
+import FeaturedProperties from "../components/FeaturedProperties";
 
 const HomePage = () => {
-  connectDb()
+  connectDb();
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </>
   );
 };
 
 export default HomePage;
-
